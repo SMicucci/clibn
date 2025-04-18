163422684d9291ceeb50e6818b068dcdff9bdb6c
Simone Micucci <s.micucci00@gmail.com>
2025-04-18 19:31:40 +0200

    feature: vector lib kinda working

---
+++ vector/src/test.c
@@ -0,0 +1,93 @@
+#include "vector.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+enum vp_mode { NONE, LABEL, DATA, ALL };
+static inline void print_vector(vector const *src, enum vp_mode mode);
+static inline void print_bits(u_int8_t trg);
+
+int main(void)
+{
+        // test vector creation
+        void *v_int = vector_new(int);
+        // add some stuff
+        printf("> push some stuff...\n");
+        vector_push(v_int, &((int){15 << 4}));
+        vector_push(v_int, &((int){15 << 4}));
+        vector_push(v_int, &((int){15 << 4}));
+        print_vector(v_int, ALL); /* PRINT */
+        // discrete insertion
+        printf("> insert other stuff...\n");
+        vector_insert(v_int, 0, &((int){15}));
+        vector_insert(v_int, 2, &((int){15}));
+        print_vector(v_int, DATA); /* PRINT */
+        // discrete extraction
+        printf("> remove newer stuff...\n");
+        free(vector_remove(v_int, 0));
+        free(vector_remove(v_int, 1));
+        print_vector(v_int, DATA); /* PRINT */
+        // wrong usage are safe?
+        printf("> do some action out of scope stuff...\n");
+        vector_insert(v_int, vector_count(v_int) + 2, &((int){15 << 4}));
+        free(vector_remove(v_int, vector_count(v_int) + 2));
+        print_vector(v_int, DATA); /* PRINT */
+        // free it
+        printf("> pop everything and beyond...\n");
+        for (int i = 0; i < 10; i++) {
+                free(vector_pop(v_int));
+        }
+        print_vector(v_int, ALL); /* PRINT */
+        vector_delete(v_int);
+        return EXIT_SUCCESS;
+}
+
+static inline void print_vector(vector const *src, enum vp_mode mode)
+{
+        if (mode & LABEL) {
+                printf("vector<%s>[%lu]: %lu\n", vector_type(src),
+                       vector_cap(src), vector_count(src));
+        }
+        if (mode & DATA) {
+                u_int64_t n = vector_count(src);
+                u_int64_t s = vector_size(src);
+                if (n) {
+                        printf("size: %lu\n - data:\n[\n", n);
+                        for (u_int64_t i = 0; i < n; i++) {
+                                u_int8_t *b_elem =
+                                    (u_int8_t *)vector_get(src, i);
+                                printf("%8lu:(", i);
+                                for (u_int64_t j = 0; j < s; j++) {
+                                        if (s > 8 && j > 2) {
+                                                break;
+                                        }
+                                        print_bits(b_elem[j]);
+                                        if (s - j - 1) {
+                                                printf("-");
+                                        }
+                                }
+                                if (s > 8) {
+                                        printf("... ");
+                                }
+                                printf(")");
+                                if (n - i - 1) {
+                                        printf(",");
+                                }
+                                printf("\n");
+                                free(b_elem);
+                        }
+                        printf("]\n");
+                }
+        }
+        return;
+}
+
+static inline void print_bits(u_int8_t trg)
+{
+        for (int8_t i = 7; i >= 0; i--) {
+                u_int8_t pos = 1 << i;
+                u_int8_t mod = trg & pos;
+                u_int8_t bit = mod ? 1 : 0;
+                printf("%d", bit);
+        }
+}
---
+++ vector/src/vector.c
@@ -0,0 +1,217 @@
+#include "vector.h"
+#include <asm-generic/errno-base.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+
+#ifndef NULL
+#define NULL 0
+#endif
+
+#ifndef VECTOR_INIT_CAP
+#define VECTOR_INIT_CAP 1
+#endif
+
+// not the proudest but i think it work
+#ifndef __ptr_assert
+#define __ptr_assert(ptr)                                                      \
+        do {                                                                   \
+                if (!ptr) {                                                    \
+                        errno = EINVAL;                                        \
+                        const int msg_len =                                    \
+                            strlen(#ptr) + strlen(strerror(errno)) + 5;        \
+                        char *err_msg = calloc(msg_len, sizeof(char));         \
+                        perror(strcat(                                         \
+                            strcat(strcat(strcat(strcat(err_msg, "("), #ptr),  \
+                                          ") "),                               \
+                                   strerror(errno)),                           \
+                            "\n\0"));                                          \
+                        free(err_msg);                                         \
+                }                                                              \
+        } while (0)
+#endif
+
+struct vector {
+        const char *type;
+        u_int64_t size;
+        u_int64_t cap, cnt;
+        void *data;
+};
+
+/**
+ * INTERNAL DECLARATION
+ */
+typedef enum _direction { FORWARD, BACKWARD } direction;
+static inline int _vector_grow(vector *this);
+static inline int _vector_shift(vector *this, u_int64_t from, direction dir);
+
+/**
+ * EXTERNAL IMPLEMENTATION
+ */
+vector *_vector_new(u_int64_t size, const char *type)
+{
+        vector *this = calloc(1, sizeof(*this));
+        char *_type = calloc(strlen(type) + 1, sizeof(char));
+        strcpy(_type, type);
+        this->type = (const char *)_type;
+        this->size = size;
+        this->cap = VECTOR_INIT_CAP;
+        this->cnt = 0;
+        this->data = calloc(VECTOR_INIT_CAP, this->size);
+        return this;
+}
+
+void vector_delete(vector *this)
+{
+        __ptr_assert(this);
+        __ptr_assert(this->data);
+        free(this->data);
+        free((void *)this->type);
+        free(this);
+        return;
+}
+
+u_int64_t vector_count(const vector *this) { return this->cnt; }
+u_int64_t vector_cap(const vector *this) { return this->cap; }
+u_int64_t vector_size(const vector *this) { return this->size; }
+const char *vector_type(const vector *this) { return this->type; }
+
+void vector_set(vector *this, u_int64_t pos, const void *val)
+{
+        __ptr_assert(this);
+        __ptr_assert(this->data);
+        __ptr_assert(val);
+        if (this->cnt < pos) {
+                errno = EINVAL;
+        }
+        memcpy(this->data + (this->size * pos), val, this->size);
+}
+
+void *vector_get(const vector *this, u_int64_t pos)
+{
+        __ptr_assert(this);
+        if (this->cnt < pos) {
+                errno = EINVAL;
+        }
+        void *val = calloc(1, this->size);
+        memcpy(val, this->data + (this->size * pos), this->size);
+        return val;
+}
+
+void vector_push(vector *this, const void *val)
+{
+        __ptr_assert(this);
+        if (_vector_grow(this)) {
+                perror(strerror(errno));
+                return;
+        }
+        memcpy(this->data + (this->cnt++ * this->size), val, this->size);
+        return;
+}
+
+void *vector_pop(vector *this)
+{
+        __ptr_assert(this);
+        if (!this->cnt) {
+                return NULL;
+        }
+        void *val = calloc(1, this->size);
+        // number of element point to the next entry
+        memcpy(val, this->data + this->size * this->cnt--, this->size);
+        memset(this->data + this->size * this->cnt, 0, this->size);
+        return val;
+}
+
+void vector_insert(vector *this, u_int64_t pos, const void *val)
+{
+        __ptr_assert(this);
+        __ptr_assert(val);
+        if (!(this->cnt - pos)) {
+                vector_push(this, val);
+                return;
+        }
+        if (_vector_grow(this)) {
+                perror(strerror(errno));
+                return;
+        }
+        this->cnt++;
+        _vector_shift(this, pos, FORWARD);
+        memcpy(this->data + this->size * pos, val, this->size);
+}
+
+void *vector_remove(vector *this, u_int64_t pos)
+{
+        __ptr_assert(this);
+        if (!(this->cnt - pos)) {
+                return vector_pop(this);
+        }
+        void *res = calloc(1, this->size);
+        memcpy(res, this->data + this->cnt * this->size, this->size);
+        this->cnt--;
+        _vector_shift(this, pos, BACKWARD);
+        return res;
+}
+
+/**
+ * INTERNAL DEFINITION
+ */
+
+/**
+ * internal check for growing memory.
+ * don't increment counter
+ */
+static inline int _vector_grow(vector *this)
+{
+        // continue when is full
+        if (this->cap - this->cnt)
+                return 0;
+        this->cap <<= 1;
+        void *__old = this->data;
+        this->data = realloc(__old, this->cap * this->size);
+        // realloc failed (NULL return)
+        if (!this->data) {
+                errno = EADDRINUSE;
+                this->cap = this->cnt = 0;
+                return -1;
+        }
+        return 0;
+}
+
+/**
+ * shift every last `(n - from)` value according to dir.
+ * FORWARD: `from -> (from + 1)` ..  `[(n - 1) -> n]`.
+ * BACKWARD: `(from + 1) -> from` .. `[(n + 1) -> n]`.
+ */
+static inline int _vector_shift(vector *this, u_int64_t from, direction dir)
+{
+        if (!this || this->cnt < from) {
+                errno = EINVAL;
+                return -1;
+        }
+        switch (dir) {
+                case FORWARD:
+                        for (u_int64_t i = this->cnt; i > from; i--) {
+                                void *dest = this->data + this->size * i;
+                                void *src = this->data + this->size * (i - 1);
+                                memcpy(dest, src, this->size);
+                        }
+                        // zeros double data
+                        memset(this->data + this->size * from, 0, this->size);
+                        return 0;
+                case BACKWARD:
+                        for (u_int64_t i = from; i < this->cnt; i++) {
+                                void *dest = this->data + this->size * i;
+                                void *src = this->data + this->size * (i + 1);
+                                memcpy(dest, src, this->size);
+                        }
+                        // zeros double data
+                        memset(this->data + this->size * (this->size + 1), 0,
+                               this->size);
+                        return 0;
+                default:
+                        errno = EINVAL;
+                        return -1;
+        }
+}
