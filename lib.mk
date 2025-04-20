68badef82265462ffa84642bb417b25df40d681d
Simone Micucci <s.micucci00@gmail.com>
2025-04-21 01:58:26 +0200

    feature: linkage on other project implemented

--- lib.mk
+++ lib.mk
@@ -2,11 +2,11 @@
 # MODIFY HERE
 #
 
-LIB_n = mylib
+LIB_n = 
 TEST = test
 BIN_n = test.out
 # directories names
-DEPS_d =
+DEPS_d = 
 INC_d = include
 SRC_d = src
 OBJ_d = obj
@@ -21,38 +21,44 @@ DBGFLAGS = -g -O0
 CPPFLAG = -I$(INC_d)
 CPPFLAG += $(foreach dep,$(DEPS_d),-I../$(dep)/$(INC_d))
 
+ifeq ($(strip $(DEPS_d)),)
+	LBFLAG = 
+else
+	LBFLAG = -L../libs $(foreach lib, $(DEPS_d), -l$(lib))
+endif
+
 SRC = $(filter-out $(SRC_d)/$(TEST).c, $(wildcard $(SRC_d)/*.c))
 OBJ = $(patsubst $(SRC_d)/%, $(OBJ_d)/%, $(SRC:.c=.o))
 
-.PHONY: dirs build debug clean port test
+.PHONY: dirs deps build debug clean port test
 
 build: dirs $(BIN_d)/lib$(LIB_n).so port
 
 debug: CFLAGS += $(DBGFLAGS)
 debug: dirs $(BIN_d)/$(BIN_n)
 
-static: dirs $(BIN_d)/lib$(LIB_n).a
-
 dirs:
 	@mkdir -p $(BIN_d) $(OBJ_d) ../libs ../include
 
-port: $(BIN_d)/lib$(LIB_n).so
-	@cp $(BIN_d)/lib$(LIB_n).so ../libs
-	@cp $(INC_d)/*.h ../include
-
-$(OBJ): $(SRC) $(DEPS)
-	$(CC) -c -fPIC $(CPPFLAG) -o $@ $< $(CFLAGS)
-
-$(BIN_d)/lib$(LIB_n).so: $(OBJ)
+deps:
 	@if [ -n "$(strip $(DEPS_d))" ]; then \
 		for dep in $(DEPS_d); do \
 			$(MAKE) -C ../$$dep build || exit 1; \
 		 done; \
 	 fi
-	$(CC) -shared -o $@ $^
 
-$(BIN_d)/$(BIN_n): src/$(TEST).c $(BIN_d)/lib$(LIB_n).a
-	$(CC) -L$(BIN_d) -Wl,-rpath,$(BIN_d) -o $@ $< -l$(LIB_n) $(CFLAGS)
+port: $(BIN_d)/lib$(LIB_n).so
+	@cp $(BIN_d)/lib$(LIB_n).so ../libs
+	@cp $(INC_d)/*.h ../include
+
+$(OBJ): $(SRC) deps
+	$(CC) -c -fPIC $(CPPFLAG) -o $@ $< $(CFLAGS)
+
+$(BIN_d)/lib$(LIB_n).so: $(OBJ)
+	$(CC) -shared -o $@ $^ $(LBFLAG)
+
+$(BIN_d)/$(BIN_n): src/$(TEST).c $(BIN_d)/lib$(LIB_n).so
+	$(CC) -L$(BIN_d) $(LBFLAG) -Wl,-rpath,$(BIN_d) -o $@ $< -l$(LIB_n) $(CFLAGS)
 
 test: debug
 	@valgrind -s $(BIN_d)/$(BIN_n)
